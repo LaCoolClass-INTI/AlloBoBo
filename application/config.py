f1fd7ce8f0b1cb9ef404be9f8f163184d1a7e917
Adrien SALIOU <adrien.saliou@aliceadsl.fr>
2022-04-08 14:38:54 +0200

    ajout de config.py

---
+++ application/config.py
@@ -0,0 +1,14 @@
+class Config :
+    MAIL_SERVER : 'smtp-relay.gmail.com'
+    MAIL_PORT :  25
+    MAIL_USE_TLS : False
+    MAIL_USE_SSL : False
+    MAIL_USERNAME : False
+    MAIL_PASSWORD : False
+
+    MAIL_DEBUG : True
+    MAIL_SUPPRESS_SEND : False
+
+    MAIL_DEFAULT_SENDER : ('Flask mailer', 'fausse.adresse@gmail.com')
+    MAIL_MAX_EMAILS : None
+    MAIL_ASCII_ATTACHMENTS : False
